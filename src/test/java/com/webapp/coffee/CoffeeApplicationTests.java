31996dc309da08013b61aa8961191e9f5188f49e
Oleh Chonka <ochonka@lenovo.com>
2020-11-12 09:24:44 +0200

    -Setting up application, connect to db,setting up flyway migration.

---
+++ src/test/java/com/webapp/coffee/CoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.coffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class CoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
